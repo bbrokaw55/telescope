447cda7976ba301be8ae45daaec2969d42b8bbfe
bbrokaw <benbrokaw55@gmail.com>
2019-10-31 13:48:51 -0600

    got code working -> has bugs -> soln is better than the optimal in rubric??

--- Xx3LIT34205N1P3RQU1CK360SC0P3xX.py
+++ Xx3LIT34205N1P3RQU1CK360SC0P3xX.py
@@ -1,36 +1,50 @@
-def startTelescope(eventList[])
-    vector<stack<time>> DP
-    DP.at(0) = null
-    For i in range eventlist
-        DP.append(new stack<time>)
-    Path = telescope(0,0, eventList[], DP)
-    return (path.size, path, traceback(path, eventList[])
 
-def telescope(time, position, eventList[], DP<stack<time>>)
-    stack Path<time>;
-    For i in range time+1:size(eventList)
-        If (abs(position) - abs(eventPos) <= eventTime-time):
-            telescope(i, event.position, eventList[])
-            If DP.at(i).size > path.size
-            Path = DP.at(i)
-    If (time != 0) { // start is not an event
-        DP.at(time) = path
-        DP.at(time).push(time)
 
-def traceback(eventList[], times/*stack of positions visited in order*/)
-    String moves = “”
-    Int cur_pos
-    Int time = 0
-    While !times.empty
-        While cur_pos < eventList[times.top]
-            moves.append(+1)
-            Cur_pos += 1
-            Time += 1
-        While cur_pos > eventList[times.top]
-            moves.append(-1)
-            Cur_pos -= 1
-        While time != times.top
-            moves.append(0)
-            Time += 1
-        Times.pop()
+def startTelescope(eventList):
+    DP = []  # vector<stack<time>> DP
+    DP.append(0)
+
+    for i in range(len(eventList)):
+        DP.append([])   #append empty list to make a 2d list
+    path = telescope(0,0, eventList, DP)
+    return (path)#, traceback(path, eventList))
+
+def telescope(time, position, eventList, DP):
+    PATH = []            # stack Path<time>;
+    for eventTime in range(time+1,len(eventList)):
+        if ((abs(position - eventList[eventTime]) <= eventTime-time)):
+            telescope(eventTime, eventList[eventTime], eventList, DP)
+            if len(DP[eventTime]) > len(PATH):
+                PATH = DP[eventTime]
+    # if (time != 0):  # start is not an event
+    DP[time] = PATH
+    DP[time].append(time)
+    print("DP at time: ", time, " --> ", DP[time])
+    return DP[time]
+
+def traceback(eventList, times):
+    moves = "{" # init empty string
+    cur_pos, time = 0,0  # initial position & time == 0
+    times.pop() # pop out the 0
+    while (len(times) != 0):
+        eventTime = times.pop()
+
+        while cur_pos < eventList[eventTime]:
+            moves += "+1, "
+            cur_pos += 1
+            time += 1
+        while cur_pos > eventList[eventTime]:
+            moves += "-1, "
+            cur_pos -= 1
+        while (time != eventTime):
+            moves += "0, "
+            time += 1
+        # times.pop()
+    moves += "}"
     return moves
+
+eventList = [0, 1,-4,-1,4,5,-4,6,7,-2]
+path = startTelescope(eventList)
+print(len(path))
+print(path)
+print(traceback(eventList, path))
