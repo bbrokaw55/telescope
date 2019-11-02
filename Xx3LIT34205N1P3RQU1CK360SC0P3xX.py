bfe16578a6ff00f9ea663c59e51149ce8b098472
bbrokaw <benbrokaw55@gmail.com>
2019-11-01 19:56:50 -0600

    finito

--- Xx3LIT34205N1P3RQU1CK360SC0P3xX.py
+++ Xx3LIT34205N1P3RQU1CK360SC0P3xX.py
@@ -2,49 +2,67 @@
 
 def startTelescope(eventList):
     DP = []  # vector<stack<time>> DP
-    DP.append(0)
+    DP.append([])   # start event
 
     for i in range(len(eventList)):
         DP.append([])   #append empty list to make a 2d list
-    path = telescope(0,0, eventList, DP)
-    return (path)#, traceback(path, eventList))
+    path = telescope(0 ,0, eventList, DP)
+    # path.pop()   # pop out the 0
+    # for num in path:
+        # print(DP[num])
+    print(len(path))
+    print(path)
+    print(traceback(eventList, path))
 
 def telescope(time, position, eventList, DP):
-    PATH = []            # stack Path<time>;
-    for eventTime in range(time+1,len(eventList)):
-        if ((abs(position - eventList[eventTime]) <= eventTime-time)):
-            telescope(eventTime, eventList[eventTime], eventList, DP)
+
+    if(len(DP[time]) != 0): return DP[time]
+    PATH = []  # stack Path<time>;
+    for eventTime in range(time+1,len(eventList)+1):
+        eventPosition = eventList[eventTime-1]
+        if time == 0 : eventPosition -= 1
+        if (abs(position - eventPosition) <= eventTime-time):
+
+            # if(len(DP[eventTime]) == 0) :
+            telescope(eventTime, eventPosition, eventList, DP)
             if len(DP[eventTime]) > len(PATH):
-                PATH = DP[eventTime]
-    # if (time != 0):  # start is not an event
-    DP[time] = PATH
-    DP[time].append(time)
-    print("DP at time: ", time, " --> ", DP[time])
+
+                PATH = DP[eventTime].copy()
+    DP[time] = PATH.copy()
+
+    if (time != 0):  # start is not an event
+        DP[time].append(time)
+    print(DP)
+    # print("DP at time: ", time, " --> ", DP[time])
+
     return DP[time]
 
 def traceback(eventList, times):
     moves = "{" # init empty string
-    cur_pos, time = 0,0  # initial position & time == 0
-    times.pop() # pop out the 0
+    cur_pos = 0
+    time = 0  # initial position & time == 0
+
     while (len(times) != 0):
         eventTime = times.pop()
-
-        while cur_pos < eventList[eventTime]:
+        eventPosition = eventList[eventTime - 1]
+        while cur_pos < eventPosition:
             moves += "+1, "
             cur_pos += 1
             time += 1
-        while cur_pos > eventList[eventTime]:
+
+        while cur_pos > eventPosition:
             moves += "-1, "
             cur_pos -= 1
+            time += 1
+
         while (time != eventTime):
             moves += "0, "
             time += 1
-        # times.pop()
-    moves += "}"
-    return moves
 
-eventList = [0, 1,-4,-1,4,5,-4,6,7,-2]
-path = startTelescope(eventList)
-print(len(path))
-print(path)
-print(traceback(eventList, path))
+    moves += "}"
+    return(moves)
+
+eventList =  [ 1,-4,-1, 4, 5, -4, 6, 7,-2]
+eventList2 = [ 0, 0, 0, 4, 3, 3, 8, -1, 6, 8]
+
+startTelescope(eventList)
